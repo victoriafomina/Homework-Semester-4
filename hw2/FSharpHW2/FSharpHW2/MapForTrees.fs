6134f1a0d8cf3502c851803dad677eaaa44a58a7
Victoria <viky.fomina@yandex.ru>
2020-04-19 00:51:25 +0300

    fixed map for trees

--- hw2/FSharpHW2/FSharpHW2/MapForTrees.fs
+++ hw2/FSharpHW2/FSharpHW2/MapForTrees.fs
@@ -4,33 +4,8 @@ type Tree<'a> =
 | Node of 'a * Tree<'a> * Tree<'a>
 | Leaf
 
-let rec mapTree tree f =
+/// <summary>Maps the result of function f to all the nodes of a tree.<\summary>
+let rec mapTree tree f = 
     match tree with 
-    | Node(v, l, r) -> f v (mapTree l f) (mapTree r f)
-
-let rec insert tree value =
-    match tree with
-    | Node(v, l, r) when value < v -> 
-        let l' = insert l value
-        Node(v, l', r)
-    | Node(v, l, r) when value > v ->
-        let r' = insert r value
-        Node(v, l, r')      
-    | Leaf -> Node(value, Leaf, Leaf)
-
-let rec isInTree tree x =
-    match tree with
-    | Node(v, l, r) when v < x -> isInTree r x
-    | Node(v, l, r) when v > x -> isInTree l x
-    | Node(v, l, r) when v = x -> true
-    | Leaf -> false
-
- // не получилось: распечатать и добавить в список
- (*  
-let rec listElements(tree: Tree<int>, list: List<int>) =
-    match tree with
-    | Node(v, l, r) -> printf "%d" v 
-        listElements(l v :: list)
-        listElements r v :: list
-*)
-    
+    | Node(v, l, r) -> Node(f v, mapTree l f, mapTree r f)
+    | Leaf -> Leaf
--- hw2/FSharpHW2/FSharpHW2Tests/MapForTreesTests.fs
+++ hw2/FSharpHW2/FSharpHW2Tests/MapForTreesTests.fs
@@ -1,2 +1,10 @@
 ﻿module MapForTreesTests
 
+open NUnit.Framework
+open FsCheck
+open MapForTrees
+open FsUnit
+
+//[<Test>]
+//let ``Test for the leaf should return a leaf`` () =
+ //   mapTree(Leaf, fun x -> x * x) |> should equal Leaf
