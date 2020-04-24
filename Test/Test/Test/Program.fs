904445d2772169f301384ad74d7a97083908eff5
Victoria <viky.fomina@yandex.ru>
2020-04-24 18:53:52 +0300

    useless usings good bye

--- Test/Test/Test/Program.fs
+++ Test/Test/Test/Program.fs
@@ -1,9 +1,4 @@
-﻿open System
-open GreatestPalindrome
-open SuperMap
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main argv =
-    printfn "%d" greatestPalindrome
-    printfn "%A" (superMap([1.0; 2.0; 3.0]))
+
     0
