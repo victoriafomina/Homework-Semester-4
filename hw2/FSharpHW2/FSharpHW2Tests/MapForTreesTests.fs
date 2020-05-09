3cde7b93d3c06988d85253174721cadb2d515553
Victoria <viky.fomina@yandex.ru>
2020-05-09 16:52:41 +0300

    added more tests

--- hw2/FSharpHW2/FSharpHW2Tests/MapForTreesTests.fs
+++ hw2/FSharpHW2/FSharpHW2Tests/MapForTreesTests.fs
@@ -1,10 +1,17 @@
 ﻿module MapForTreesTests
 
 open NUnit.Framework
-open FsCheck
 open MapForTrees
 open FsUnit
 
-//[<Test>]
-//let ``Test for the leaf should return a leaf`` () =
- //   mapTree(Leaf, fun x -> x * x) |> should equal Leaf
+[<Test>]
+let ``Test the leaf should return a leaf`` () =
+    mapTree Leaf (fun x -> x * x) |> should equal Leaf
+
+[<Test>]
+let ``Test one Node should return a Node`` () =
+    mapTree (Node(1, Leaf, Leaf)) (fun x -> x * x) |> should equal (Node(1, Leaf, Leaf))
+
+[<Test>]
+let ``Test tree should return a tree`` () =
+    mapTree (Node(-1, Node(2, Node(-5, Leaf, Leaf), Node(-7, Leaf, Leaf)), Leaf)) (fun x -> abs(x)) |> should equal (Node(1, Node(2, Node(5, Leaf, Leaf), Node(7, Leaf, Leaf)), Leaf))
