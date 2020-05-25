5e74e54cafe0d26f7da4128864069afb948328b3
Victoria <viky.fomina@yandex.ru>
2020-05-25 21:09:25 +0300

    Project is ready. I have to make tests

---
+++ hw4/Point-free/Point-free/PointFree.fs
@@ -0,0 +1,14 @@
+﻿module PointFree
+
+// Записать в point-free стиле func x l = List.map (fun y -> y * x) l. 
+// Выписать шаги вывода и проверить с помощью FsCheck корректность результата
+
+let multiply1 x l = List.map (fun y -> y * x) l
+
+let multiply2 x = List.map (fun y -> y * x)
+
+let multiply3 x = List.map (*) x
+
+let multiply4 = List.map (*)
+
+// let x l = List.map (*) x l
