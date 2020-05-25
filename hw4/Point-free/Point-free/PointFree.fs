35216ab046829f94c36ff1078b3ca4c78016cb0f
Victoria <viky.fomina@yandex.ru>
2020-05-25 22:47:29 +0300

    comments are fixed

--- hw4/Point-free/Point-free/PointFree.fs
+++ hw4/Point-free/Point-free/PointFree.fs
@@ -8,5 +8,5 @@ let multiply2 x = List.map (fun y -> y * x)
 
 let multiply3 x = List.map << (*) <| x
 
-// Point-free version. Multiplies the elements of the list.
+/// Point-free version. Multiplies the elements of the list.
 let multiplyPointFree = List.map << (*)
