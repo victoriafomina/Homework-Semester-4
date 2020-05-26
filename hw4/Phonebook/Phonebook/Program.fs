76b5d45f8983fcdcf954db9b9951dded226282d6
Victoria <viky.fomina@yandex.ru>
2020-05-26 12:09:00 +0300

    file was found

--- hw4/Phonebook/Phonebook/Program.fs
+++ hw4/Phonebook/Phonebook/Program.fs
@@ -2,10 +2,12 @@
 
 open System
 open PhonebookLogic
+open System.IO
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" (List.head (List.head (readInfoFromFile "Phonebook.txt" [])))
+    printfn "%A" (List.head (List.head (readInfoFromFile "..\\..\\..\\Phonebook.txt" [])))
+    printfn "%s" (Directory.GetCurrentDirectory())
 
 
     0 // return an integer exit code
