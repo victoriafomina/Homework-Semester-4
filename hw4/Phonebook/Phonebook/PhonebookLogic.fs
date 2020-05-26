13c0ab3f3a62fcc3c88bdd6900ce9b5cdc3f3478
Victoria <viky.fomina@yandex.ru>
2020-05-26 04:18:38 +0300

    Implemented reading from file

---
+++ hw4/Phonebook/Phonebook/PhonebookLogic.fs
@@ -0,0 +1,27 @@
+﻿module PhonebookLogic
+
+open System.IO
+open Microsoft.FSharp.Core.Operators
+
+(*Написать программу - телефонный справочник. Она должна уметь хранить имена и номера 
+телефонов, в интерактивном режиме осуществлять следующие операции:
+1. выйти
+2. добавить запись (имя и телефон)
+3. найти телефон по имени
+4. найти имя по телефону
+5. вывести всё текущее содержимое базы
+6. сохранить текущие данные в файл
+7. считать данные из файла
+*)
+
+/// Reads the notes from file.
+let readInfoFromFile (fileName : string) store =
+    let spaceIndex = Seq.findIndex(fun x -> x = ' ')
+    
+    let splitIntoListOfStrings (str : string) = [str.[..spaceIndex str]; str.[((spaceIndex str) + 1)..]]
+
+    try
+        if store = [] then
+            ((fileName |> File.ReadLines).ToString() |> splitIntoListOfStrings) :: store
+        else store
+    with | _ -> failwith "File was not open!"
---
+++ hw4/Phonebook/Phonebook/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open PhonebookLogic
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (List.head (List.head (readInfoFromFile "Phonebook.txt" [])))
+
+
+    0 // return an integer exit code
