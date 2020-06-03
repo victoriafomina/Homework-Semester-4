d7a3f8479d7c2bf018aae7c138dcf5a6cff70252
Victoria <viky.fomina@yandex.ru>
2020-06-03 16:10:12 +0300

    added tests

--- hw6/WorkflowCalculateStrings/WorkflowCalculateStrings/CalculateStrings.fs
+++ hw6/WorkflowCalculateStrings/WorkflowCalculateStrings/CalculateStrings.fs
@@ -2,6 +2,7 @@
 
 open System
 
+/// Implements interface letting calculate strings of integer numbers.
 type CalculateBuilder() =
     member this.Bind(x : string, f) =
         if Seq.length x = ((Seq.filter (fun c -> Char.IsDigit(c)) x) |> Seq.length) then (f <| Int32.Parse(x)).ToString() else "None"
