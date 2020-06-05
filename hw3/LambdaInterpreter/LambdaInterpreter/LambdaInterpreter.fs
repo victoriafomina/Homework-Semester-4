7c6c69f5d37f01a8969aa2183e076ce22d7cc1fc
Victoria <viky.fomina@yandex.ru>
2020-06-05 23:41:45 +0300

    Added definition of type lambda term. Now I can check if var is free in term

--- hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
+++ hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
@@ -1,2 +1,18 @@
 ﻿module LambdaInterpreter
 
+// Реализовать интерпретатор лямбда-выражений, выполняющий бета-редукцию по нормальной стратегии. 
+// Лямбда-выражения задаются через размеченные объединения. Должна поддерживаться альфа-конверсия для 
+// избежания захвата свободных переменных. Примечание: если не извращаться и делать всё по определению, задача простая.
+
+/// Defines lambda term type.
+type LambdaTerm =
+    | Variable of char
+    | Abstr of char * LambdaTerm
+    | Applic of LambdaTerm * LambdaTerm
+
+/// Checks if variable is free.
+let rec isFreeVar expression var = 
+    match expression with 
+    | Variable x -> x = var
+    | Abstr(vr, term) -> vr = var || (isFreeVar term var) 
+    | Applic(l, r) -> isFreeVar l var || isFreeVar r var
