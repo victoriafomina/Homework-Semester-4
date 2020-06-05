dc92d2114c750731bb185a45acf8d051b3b35c5e
Victoria <viky.fomina@yandex.ru>
2020-06-05 19:30:48 +0300

    Seems ready. Tests are left

--- hw7/WebPages/WebPages/WebPagesLinks.fs
+++ hw7/WebPages/WebPages/WebPagesLinks.fs
@@ -2,32 +2,22 @@
 
 open System.Net
 open System.IO
-open System.Threading
-open System
 open System.Text.RegularExpressions
 
-(*
-let sites = ["http://se.math.spbu.ru"; "http://spisok.math.spbu.ru"]
-let fetchAsync url =
-    async {
-        do printfn "Creating request for %s..." url
-        let request = WebRequest.Create(url)
-        use! response = request.AsyncGetResponse()
-        do printfn "Getting response stream for %s..." url
-        use stream = response.GetResponseStream()
-        do printfn "Reading response for %s..." url
-        use reader = new StreamReader(stream)
-        let html = reader.ReadToEnd()
-        do printfn "Read %d characters for %s..." html.Length url
-    }
-    sites |> List.map (fun site -> site |> fetchAsync |> Async.Start) |> ignore
- *)
-
+/// Downloads all the contens the link links.
 let download (link : string) =
+    /// Converts list of matches to list of lists.
+    let makeListOfLists (matches : MatchCollection) =
+        let rec makeListOfMatchesRec acc (matches : MatchCollection) list =
+            if acc = matches.Count then list
+            else
+                makeListOfMatchesRec (acc + 1) matches (matches.[acc].Value :: list)
+        makeListOfMatchesRec 0 matches [] 
+
     /// Finds all the links on the page.
-    let findsLinks page =
+    let findLinks page =
         let regex = new Regex("<a href=\"http[:]{1}[/]{2}(.*?)\">")
-        regex.Matches(page) |> Seq.toList
+        regex.Matches(page) |> makeListOfLists
         
     /// Downloads info from the url asynchonously.
     let fetchAsync (url : string) =        
@@ -39,39 +29,10 @@ let download (link : string) =
             let html = reader.ReadToEnd()
             printfn "Downloaded from: %s" url
             printfn "Number of symbols: %d" html.Length 
+            return html
             }
-
-    async {
-        
-        let regex = new Regex("<a href=\"http[:]{1}[/]{2}(.*?)\">")
-        let matches = regex.Matches(html)
-        let makeListOfMatches (matches : MatchCollection) =
-            let rec makeListOfMatchesRec acc (matches : MatchCollection) list =
-                if acc = matches.Count then list
-                else
-                    makeListOfMatchesRec (acc + 1) matches (matches.[acc].Value :: list)
-            makeListOfMatchesRec 0 matches [] 
-
-
-        makeListOfMatches matches
-        |> List.map fetchUrlAsync  // make a list of async tasks
-        |> Async.Parallel          // set up the tasks to run in parallel
-        |> Async.RunSynchronously
-            
-            
-            (*
-            // a list of sites to fetch
-            let sites = ["http://www.bing.com";
-                         "http://www.google.com";
-                         "http://www.microsoft.com";
-                         "http://www.amazon.com";
-                         "http://www.yahoo.com"]
-            
-            #time                      // turn interactive timer on
-            sites 
-            |> List.map fetchUrlAsync  // make a list of async tasks
-            |> Async.Parallel          // set up the tasks to run in parallel
-            |> Async.RunSynchronously  // start them off
-            #time 
-            *)
-    }
+    
+    /// Downloads the content from the links parallel.
+    (link |> fetchAsync).ToString() 
+    |> findLinks 
+    |> List.map fetchAsync |> Async.Parallel |> Async.RunSynchronously |> Seq.toList
