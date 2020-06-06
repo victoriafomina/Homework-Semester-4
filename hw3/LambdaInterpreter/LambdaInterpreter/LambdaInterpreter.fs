36c4b3bc2c37eb0fea786e7ba05893bf15a12fe2
Victoria <viky.fomina@yandex.ru>
2020-06-06 18:50:26 +0300

    seems ready

--- hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
+++ hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
@@ -11,7 +11,6 @@ type LambdaTerm =
     | Applic of LambdaTerm * LambdaTerm
 
 
-
 /// Beta reduction. Normal strategy.
 let rec betaReduction expression =
 
@@ -22,6 +21,10 @@ let rec betaReduction expression =
         | Abstr(vr, term) -> vr <> var && (isFreeVar term var) 
         | Applic(l, r) -> isFreeVar l var || isFreeVar r var
 
+    /// Conversion.
+    let findVarToConverse expression =
+        List.find (fun x -> not (isFreeVar expression x)) ['a'..'z']
+
     /// Makes substitution.
     let rec substitute expression insteadOf value =
         match expression with
@@ -29,6 +32,9 @@ let rec betaReduction expression =
         | Variable -> expression
         | Abstr(var, lmd) when var = insteadOf -> expression
         | Abstr(var, lmd) when not (isFreeVar value var) -> Abstr(var, substitute lmd insteadOf value)
+        | Abstr(var, lmd) -> 
+            let right = substitute (substitute lmd var (Variable (findVarToConverse expression))) insteadOf value
+            Abstr(findVarToConverse(value), right)
         | Applic(l, r) -> Applic(substitute l insteadOf value, substitute r insteadOf value)
         | _ -> expression
 
