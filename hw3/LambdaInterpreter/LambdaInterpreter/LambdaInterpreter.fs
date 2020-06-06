b05fe78cf1fd3a7648194f44758f77a3d8a99195
Victoria <viky.fomina@yandex.ru>
2020-06-06 14:50:10 +0300

    Fixed an awful thing. Kak booshe testi prohodili???

--- hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
+++ hw3/LambdaInterpreter/LambdaInterpreter/LambdaInterpreter.fs
@@ -10,37 +10,40 @@ type LambdaTerm =
     | Abstr of char * LambdaTerm
     | Applic of LambdaTerm * LambdaTerm
 
-/// Checks if variable is free.
-let rec isFreeVar expression var = 
-    match expression with 
-    | Variable x -> x = var
-    | Abstr(vr, term) -> vr <> var && (isFreeVar term var) 
-    | Applic(l, r) -> isFreeVar l var || isFreeVar r var
 
-/// Makes substitution.
-let rec substitute expression insteadOf value =
-    match expression with
-    | Variable x when x = insteadOf -> value
-    | Variable -> expression
-    | Abstr(var, lmd) when var = insteadOf -> expression
-    | Abstr(var, lmd) when not (isFreeVar value var) -> Abstr(var, substitute lmd insteadOf value)
-    | Applic(l, r) -> Applic(substitute l insteadOf value, substitute r insteadOf value)
-    | _ -> expression
-
-/// Reduce with left outer term.
-let rec reduceLeftOuter expression =
-    match expression with
-    | Applic(l, r) -> match reduceLeftOuter l with 
-                      | Abstr(var, lmd) -> substitute lmd var r 
-                      | var -> Applic(var, r)
-    | _ -> expression
 
 /// Beta reduction. Normal strategy.
 let rec betaReduction expression =
+
+    /// Checks if variable is free.
+    let rec isFreeVar expression var = 
+        match expression with 
+        | Variable x -> x = var
+        | Abstr(vr, term) -> vr <> var && (isFreeVar term var) 
+        | Applic(l, r) -> isFreeVar l var || isFreeVar r var
+
+    /// Makes substitution.
+    let rec substitute expression insteadOf value =
+        match expression with
+        | Variable x when x = insteadOf -> value
+        | Variable -> expression
+        | Abstr(var, lmd) when var = insteadOf -> expression
+        | Abstr(var, lmd) when not (isFreeVar value var) -> Abstr(var, substitute lmd insteadOf value)
+        | Applic(l, r) -> Applic(substitute l insteadOf value, substitute r insteadOf value)
+        | _ -> expression
+
+    /// Reduce with left outer term (normal strategy).
+    let rec reduceLeftOuter expression =
+        match expression with
+        | Applic(l, r) -> match reduceLeftOuter l with 
+                          | Abstr(var, lmd) -> substitute lmd var r 
+                          | var -> Applic(var, r)
+        | _ -> expression
+
     match expression with
     | Variable -> expression
     | Applic(l, r) -> match reduceLeftOuter l with 
-                      | Abstr(var, lmd) -> betaReduction (substitute lmd var r)
-                      | var -> Applic(betaReduction var, betaReduction r)
+                        | Abstr(var, lmd) -> betaReduction (substitute lmd var r)
+                        | var -> Applic(betaReduction var, betaReduction r)
     | Abstr(var, lmd) -> Abstr(var, betaReduction lmd)
     
