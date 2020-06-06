f2feec45d7ae6790977946de1aabfc2774e2c504
Victoria <viky.fomina@yandex.ru>
2020-06-06 15:38:50 +0300

    Added comments

--- FinalTest/FinalTest/FinalTest/EvenFibonacci.fs
+++ FinalTest/FinalTest/FinalTest/EvenFibonacci.fs
@@ -1,2 +1,8 @@
-﻿module Fibonacci
+﻿module EvenFibonacci
 
+/// Sum of even fibonacci numbers that are lower than a million.
+let evenFibonacci =
+    let rec fibonacci prev cur acc =
+        if cur < 1000000 then fibonacci cur (prev + cur) (acc + cur)
+        else acc
+    fibonacci 1 1 1
