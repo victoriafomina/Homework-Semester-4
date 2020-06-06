7d39cb09f08eff63593970545d2c121870a06426
Victoria <viky.fomina@yandex.ru>
2020-06-06 17:39:27 +0300

    small fix

--- FinalTest/FinalTest/FinalTestTests/QueueTests.fs
+++ FinalTest/FinalTest/FinalTestTests/QueueTests.fs
@@ -13,21 +13,21 @@ let ``Enqueue and peek test 1`` () =
 
 [<Test>]
 let ``Enqueue and peek test 2`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(1)
     q.Enqueu(1)
     q.Peek() |> should equal 1
 
 [<Test>]
 let ``Enqueue and peek test 3`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(1)
     q.Enqueu(2)
     q.Peek() |> should equal 1
 
 [<Test>]
 let ``Enqueue and peek test 4`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(5)
     q.Enqueu(-7)
     q.Enqueu(0)
@@ -35,7 +35,7 @@ let ``Enqueue and peek test 4`` () =
 
 [<Test>]
 let ``Dequeue and peek test 1`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(1)
     q.Dequeu()
     q.Enqueu(1)
@@ -43,24 +43,24 @@ let ``Dequeue and peek test 1`` () =
 
 [<Test>]
 let ``Dequeue from empty queue test 1`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     (fun () -> q.Dequeu() |> ignore) |> should throw typeof<Exception>
 
 [<Test>]
 let ``Dequeue from empty queue test 2`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(-9)
     q.Dequeu() |> ignore
     (fun () -> q.Dequeu() |> ignore) |> should throw typeof<Exception>
 
 [<Test>]
 let ``Peek with empty queue test 1`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     (fun () -> q.Peek() |> ignore) |> should throw typeof<Exception>
 
 [<Test>]
 let ``Peek with empty queue test 2`` () =
-    let q = new Queue()
+    let q = new Queue<int>()
     q.Enqueu(-9)
     q.Dequeu() |> ignore
     (fun () -> q.Peek() |> ignore) |> should throw typeof<Exception>
